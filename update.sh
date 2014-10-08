624fb1c1d1bbe1a68e0915ba79ebcc56c7cd0b3f
Kevin R. Thornton <krthornt@uci.edu>
2014-10-08 09:56:34 -0700

    more pages

--- update.sh
+++ update.sh
@@ -3,7 +3,26 @@
 #Process the header
 pandoc -S -s -c pandoc.css header.md -o header.html
 
-cd markdown
+cd Data
+
+for i in *.md
+do
+    n=`basename $i .md`
+    echo processing $i
+    pandoc -S -s -c ../pandoc.css -H ../header.html -o $n.html $i
+done
+
+cd ../software
+
+for i in *.md
+do
+    n=`basename $i .md`
+    echo processing $i
+    pandoc -S -s -c ../pandoc.css -H ../header.html -o $n.html $i
+done
+
+
+cd ../markdown
 
 for i in *.md
 do
