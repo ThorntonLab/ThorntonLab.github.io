eba5a8daad747bda7c348acb8a0c1ed47eac0307
Kevin R. Thornton <krthornt@uci.edu>
2014-10-08 08:30:49 -0700

    links

--- update.sh
+++ update.sh
@@ -9,15 +9,17 @@ for i in *.md
 do
     n=`basename $i .md`
     echo processing $i
-    pandoc -S -s -c ../pandoc.css -H ../header.html -o ../$n.html $i	
+    pandoc -S -s -c ../pandoc.css -H ../header.html -o ../$n.html $i
 done
+
+cd ..
 
 
 #cd ../blogs
 
 #for i in *.md; 
 #do
-	pandoc -S -s -c ../pandoc.css -H ../header.html -o ./${i//md/html} $i	
+#	pandoc -S -s -c ../pandoc.css -H ../header.html -o ./${i//md/html} $i	
 #	git add ./${i//md/html}
 #done
 
